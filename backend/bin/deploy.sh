c16b433ffa16db8653ee33a1499b836ff48d2de1
MrAhmedElsayed <ahmedsayed551991@gmail.com>
2022-05-20 23:20:18 +0300

    add config

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,1 +1,5 @@
 aws --version
+#Compress-Archive in.txt out.zip
+#aws s3 cp deploy.zip s3://mybucket/deploy.zip
+#aws elasticbeanstalk create-application-version --application-name my-app --version-label 12345 --source-bundle S3Bucket="mybucket",S3Key="deploy.zip"
+#aws elasticbeanstalk update-environment --application-name my-app --environment-name MyApp-env --version-label 12345
